940afbe2d5e91481fd0d5e944ce6dbdf1e9270bd
bojan <bojan.milanovic@student.etf.unibl.org>
2024-12-25 12:41:38 +0100

    promjena ikonice prozora i boje prozora

--- pythonProject/ulaz_i_izlaz.py
+++ pythonProject/ulaz_i_izlaz.py
@@ -9,6 +9,12 @@ root = tk.Tk()
 root.title("Ulaz i izlaz")
 root.geometry("400x300")  # Širina x Visina
 
+# Postavljanje ikone prozora
+root.iconphoto(False, tk.PhotoImage(file="parking.png"))
+
+# Promjena boje pozadine prozora na neutralno plavu
+root.config(bg="#A9CFE8")  # Ovdje možete promijeniti boju
+
 # Funkcija za generisanje nasumičnog jedinstvenog koda
 def generate_unique_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
@@ -26,9 +32,9 @@ def show_error(message):
     error_window = tk.Toplevel(root)
     error_window.title("Greška")
     error_window.geometry("300x150")
-    error_label = tk.Label(error_window, text=message, fg="red")
+    error_label = tk.Label(error_window, text=message, fg="red", bg="#A9CFE8")  # Pozadina greške prozora
     error_label.pack(pady=20)
-    close_button = tk.Button(error_window, text="Zatvori", command=error_window.destroy)
+    close_button = tk.Button(error_window, text="Zatvori", command=error_window.destroy, bg="#A9CFE8")
     close_button.pack(pady=10)
 
 # Funkcija za rukovanje unosom registarskih oznaka
@@ -66,7 +72,7 @@ def handle_input():
         entry.delete(0, tk.END)
 
 # Dodavanje labela za unos registarskih oznaka
-label = tk.Label(root, text="Unesite registarsku oznaku:")
+label = tk.Label(root, text="Unesite registarsku oznaku:", bg="#A9CFE8")
 label.pack(pady=10)
 
 # Dodavanje polja za unos
